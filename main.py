c314d4b3b41a2d9df3966f544c772159d156eaa3
AppleVegas <applevegass@gmail.com>
2024-12-20 03:17:27 +0300

    Oh my god why

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from database import DataBase
 
 
@@ -7,19 +7,25 @@ db = DataBase("databases\\db1.db")
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def table():
     title = db.get_db_title()
     columns = db.get_columns()
     human = db.get_human()
     human_columns = human[0]
     human_measures = human[1]
-    elements = db.get_all()
     return render_template('table.html', 
                             title=title, 
                             columns=columns, 
                             human_columns=human_columns, 
-                            human_measures=human_measures,
-                            elements=elements)
+                            human_measures=human_measures)
 
 
+@app.route('/api/data')
+def data():
+    elements = db.get_all()
+    start = request.args.get('start', type=int)
+    length = request.args.get('length', type=int)
+    lengthf = request.args.get('test', type=str)
+    print(lengthf)
+    return {'data': [dict(row) for row in elements]}
 app.run(debug=True)
